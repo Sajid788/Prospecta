b61fb9d43e3c093200227a67141745c0bd3d9168
Sajid788 <mdsajidalam8340@gmail.com>
2024-12-09 20:47:43 +0530

    bug fixed

--- coding/api/index.js
+++ coding/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const productRoutes = require('./productRoutes');
 
-const PORT = process.env.PORT || 8080;
+const PORT = 8080;
 
 const app = express();
 app.use(bodyParser.json());
